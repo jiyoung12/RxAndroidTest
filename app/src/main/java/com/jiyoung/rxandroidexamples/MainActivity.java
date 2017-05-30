73e86f3f348ab9665e23eee3b5f6ec788bc2398f
Jiyoung <Jiyoung@hongjiyeong-ui-MacBook-Pro-2.local>
2017-05-30 10:42:47 +0900

    modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/jiyoung/rxandroidexamples/MainActivity.java 	modified:   app/src/main/res/layout/activity_main.xml 	app/src/main/java/com/jiyoung/rxandroidexamples/ClickListener.java 	app/src/main/java/com/jiyoung/rxandroidexamples/ExampleActivityAndName.java 	app/src/main/java/com/jiyoung/rxandroidexamples/ExampleAdapter.java 	app/src/main/java/com/jiyoung/rxandroidexamples/example/ 	app/src/main/java/com/jiyoung/rxandroidexamples/utils/ 	app/src/main/res/drawable-v21/ 	app/src/main/res/drawable/ 	app/src/main/res/layout/activity_example1.xml 	app/src/main/res/layout/activity_example2.xml 	app/src/main/res/layout/item_menu.xml

--- app/src/main/java/com/jiyoung/rxandroidexamples/MainActivity.java
+++ app/src/main/java/com/jiyoung/rxandroidexamples/MainActivity.java
@@ -1,13 +1,63 @@
 package com.jiyoung.rxandroidexamples;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.jiyoung.rxandroidexamples.example.Example1Activity;
+import com.jiyoung.rxandroidexamples.example.Example2Activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ClickListener{
+
+    private RecyclerView recyclerView;
+    private ExampleAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBar();
+        setupExampleList();
+    }
+
+    private void setActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle("RxAndroid Examples");
+        }
+    }
+
+    private void setupExampleList(){
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ExampleAdapter(this, getList());
+        recyclerView.setAdapter(adapter);
+        adapter.setClickListener(this);
+
+    }
+
+    private List<ExampleActivityAndName> getList(){
+
+        List<ExampleActivityAndName> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleActivityAndName(Example1Activity.class, "Button Counter"));
+        exampleList.add(new ExampleActivityAndName(Example2Activity.class, "Retro Test"));
+
+        return exampleList;
+
+    }
+
+    @Override
+    public void itemClicked(View view, int position) {
+
+        Intent intent = new Intent(MainActivity.this, getList().get(position).mActivityClass);
+        startActivity(intent);
     }
 }
